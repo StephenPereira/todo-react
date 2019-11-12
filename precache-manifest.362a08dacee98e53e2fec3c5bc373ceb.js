efd60890ba22f966dfd806724e42864f7c06359e
Stephen Pereira <stephen.pereira@publicissapient.com>
2019-11-12 14:37:46 -0500

    Updates

---
+++ precache-manifest.362a08dacee98e53e2fec3c5bc373ceb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e46faf8f56f652988a31bd13155860bd",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "e0c076de38c5cd17b286",
+    "url": "/todo-react/static/css/main.8693c2fa.chunk.css"
+  },
+  {
+    "revision": "7ec5984bb64af56cd4d4",
+    "url": "/todo-react/static/js/2.715a78c3.chunk.js"
+  },
+  {
+    "revision": "e0c076de38c5cd17b286",
+    "url": "/todo-react/static/js/main.bc537d74.chunk.js"
+  },
+  {
+    "revision": "f4b5c1c7a684c9a9a7e7",
+    "url": "/todo-react/static/js/runtime-main.2232a24c.js"
+  }
+]);
